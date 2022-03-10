afb4d6981358055925bb4c17cce2ca6cbe7891a7
Hugh Cayless <philomousos@gmail.com>
2022-03-10 16:12:03 -0500

    Removed leftover cruft in action script.

--- .github/actions/publish/process.sh
+++ .github/actions/publish/process.sh
@@ -7,16 +7,6 @@ convert() {
     mkdir "pub/$DIR"
   fi
   saxon -s:"$1" -xsl:xslt/publish.xsl -o:"pub/$DIR/index.html"
-
-  OUT1=$(echo $1 | sed 's/\([[:digit:]]*\).docx/\1.xml/')
-  OUT2=$(echo $OUT1 | sed "s/docx\//articles\//")
-  if [ ! -f "$OUT2" ]; then
-    /opt/stylesheets/bin/docxtotei $f "$OUT1"
-    saxon -s:"$OUT1" -xsl:xslt/process-$1.xsl -o:"$OUT2"
-    if [ -f "$OUT1" ]; then
-      rm "$OUT1"
-    fi
-  fi
 }
 
 while (( "$#" )); do
