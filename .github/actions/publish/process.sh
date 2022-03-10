8e8909ae0b8d1e239cb034632261cd87000bdb87
Hugh Cayless <philomousos@gmail.com>
2022-03-10 16:30:44 -0500

    Fix process.sh

--- .github/actions/publish/process.sh
+++ .github/actions/publish/process.sh
@@ -3,13 +3,14 @@
 convert() {
   echo $1
   DIR=$(basename $1 | sed 's/\.xml//')
-  if [ ! -d "pub/$DIR" ]
+  if [ ! -d "pub/$DIR" ]; then
     mkdir "pub/$DIR"
   fi
   saxon -s:"$1" -xsl:xslt/publish.xsl -o:"pub/$DIR/index.html"
 }
 
 while (( "$#" )); do
+  echo "Processing file $1"
   convert $1
   shift
 done  
