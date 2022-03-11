274d080aa7b61dab936b1353a82cb67400368b29
Hugh Cayless <philomousos@gmail.com>
2022-03-11 08:45:14 -0500

    Use environment instead of args.

--- .github/actions/publish/process.sh
+++ .github/actions/publish/process.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 convert() {
-  echo $1
-  DIR=$(basename $1 | sed 's/\.xml//')
+  echo "Converting $1"
+  DIR=$(basename "$1" | sed 's/\.xml//')
   if [ ! -d "pub/$DIR" ]; then
     mkdir "pub/$DIR"
   fi
   saxon -s:"$1" -xsl:xslt/publish.xsl -o:"pub/$DIR/index.html"
 }
 
-while (( "$#" )); do
+while (( "$FILES" )); do
   echo "Processing file $1"
-  convert $1
+  if [ -n "$($1 | grep "sources/.*\.xml")" ]; then
+    convert "$1"
+  fi
   shift
 done  
